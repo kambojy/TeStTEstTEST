fe938ca196a49cb51a583fd01ff38334f7cfb499
kambojy <36192453+kambojy@users.noreply.github.com>
2018-02-15 20:49:45 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -54,7 +54,7 @@ AUTO = {
 			}
 		};
 		var gus = function(){
-if(comp.rival !== undefined){var crcu = comp.rival.car.uid;}else{var crcu = myUid*1;}
+if(typeof comp.rival !== undefined){var crcu = comp.rival.car.uid;}else{var crcu = myUid*1;}
 raceWithFriend ? raceWithFriend : false;
 //console.info(raceWithFriend+' _____ '+crcu);
 
