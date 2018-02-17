b0390f865131997656335b2fe3b353d17eb08224
kambojy <36192453+kambojy@users.noreply.github.com>
2018-02-17 15:39:59 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -93,7 +93,7 @@ if(crcu == myUid*1){acsel = true;jetOn = true;}
 		query={};query.head = 'cancelRandomRace';socket.send(JSON.stringify(query));$("#blockBox").css('display','none');
 		win2 = user.allWin - this.win1;
 		lose2 = user.allLose - this.lose1;
-		infoMsg('<center>Работа бота остановлена', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <font color=yellow>'+user.rate+'</font><br>Побед за неделю : <font color=yellow>'+user.wins+mTOP'</font> <font color=gray>________________________</font><br><font color=aqua>Бот работал : '+time.toFixed(2)+' минут.</font>');
+		infoMsg('<center>Работа бота остановлена', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <font color=yellow>'+user.rate+'</font><br>Побед за неделю : <font color=yellow>'+user.wins+mTOP+'</font> <font color=gray>________________________</font><br><font color=aqua>Бот работал : '+time.toFixed(2)+' минут.</font>');
 		}
     },
 	
@@ -106,7 +106,7 @@ if(crcu == myUid*1){acsel = true;jetOn = true;}
 		
 		win2 = user.allWin - this.win1;
 		lose2 = user.allLose - this.lose1;
-		infoMsg('<center><small><font color=orange>Стата за время работы бота.</font></small>', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <font color=yellow>'+user.rate+'</font><br>Побед за неделю : <font color=yellow>'+user.wins+mTOP'</font> <font color=gray>________________________</font><br><font color=aqua>Бот работает : '+time.toFixed(2)+' минут.</font>');
+		infoMsg('<center><small><font color=orange>Стата за время работы бота.</font></small>', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <font color=yellow>'+user.rate+'</font><br>Побед за неделю : <font color=yellow>'+user.wins+mTOP+'</font> <font color=gray>________________________</font><br><font color=aqua>Бот работает : '+time.toFixed(2)+' минут.</font>');
 	}
     
 };
