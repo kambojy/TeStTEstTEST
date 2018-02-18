8fd310aa2f718cb8fe6bdce47abd620b2421349d
kambojy <36192453+kambojy@users.noreply.github.com>
2018-02-18 12:28:04 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -96,7 +96,7 @@ if(crcu == myUid*1){acsel = true;jetOn = true;}
 		query={};query.head = 'cancelRandomRace';socket.send(JSON.stringify(query));$("#blockBox").css('display','none');
 		win2 = user.allWin - this.win1;
 		lose2 = user.allLose - this.lose1;
-		infoMsg('<center>Работа бота остановлена', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <span title="" onclick="AUTO.top100()"><font color=yellow>'+user.rate+mRAIT+'</font></span><br><span title="" onclick="AUTO.week()">Побед за неделю : <font color=yellow>'+user.wins+mTOP+'</font></span> <font color=gray>________________________</font><br><font color=aqua>Бот работал : '+time.toFixed(2)+' минут.</font>');
+		infoMsg('<center>Работа бота остановлена', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <span title="Нажать, чтобы посмотреть игроков рядом" onclick="AUTO.top100()"><font color=yellow>'+user.rate+mRAIT+'</font></span><br><span title="Нажать, чтобы посмотреть игроков рядом" onclick="AUTO.week()">Побед (неделя) : <font color=yellow>'+user.wins+mTOP+'</font></span> <font color=gray>________________________</font><br><font color=aqua>Бот работал : '+time.toFixed(2)+' минут.</font>');
 		}
     },
 	
@@ -112,7 +112,7 @@ if(crcu == myUid*1){acsel = true;jetOn = true;}
 		
 		win2 = user.allWin - this.win1;
 		lose2 = user.allLose - this.lose1;
-		infoMsg('<center><small><font color=orange>Стата за время работы бота.</font></small>', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <span title="" onclick="AUTO.top100()"><font color=yellow>'+user.rate+mRAIT+'</font></span><br><span title="" onclick="AUTO.week()">Побед за неделю : <font color=yellow>'+user.wins+mTOP+'</font></span> <font color=gray>________________________</font><br><font color=aqua>Бот работает : '+time.toFixed(2)+' минут.</font>');
+		infoMsg('<center><small><font color=orange>Стата за время работы бота.</font></small>', 'Побед : <font color=green>' + win2 + ' <small>('+user.allWin+')</small></font><br>Поражений : <font color=red>' + lose2 + ' <small>('+user.allLose+')</small></font> <font color=gray>________________________</font><br>Рейтинг : <span title="Нажать, чтобы посмотреть игроков рядом" onclick="AUTO.top100()"><font color=yellow>'+user.rate+mRAIT+'</font></span><br><span title="Нажать, чтобы посмотреть игроков рядом" onclick="AUTO.week()">Побед (неделя) : <font color=yellow>'+user.wins+mTOP+'</font></span> <font color=gray>________________________</font><br><font color=aqua>Бот работает : '+time.toFixed(2)+' минут.</font>');
 	},
 	
 	week : function(){
@@ -123,12 +123,12 @@ if(crcu == myUid*1){acsel = true;jetOn = true;}
 				qs += '<small>'+(parseInt(k)+1)+'.&nbsp;</small>'+TOP[k]['name']+'&nbsp;-&nbsp;'+TOP[k]['wins']+'<br>';
 			}
 		}
-		infoMsg(qs,'Топ недели');
+		infoMsg(qs,'<center>Топ недели');
 	},
 	top100 : function(){
 		var qs = '';
 		for(k in raiting){
-			if(raiting[k]['id'] == myUid*1){var mRAIT= parseInt(k); break}else{var mRAIT=95;}
+			if(raiting[k]['id'] == myUid*1){var mRAIT= parseInt(k); break}else{var mRAIT=93;}
 		}
 		if(mRAIT>=3){mRAIT = mRAIT-3;}else{mRAIT = 0}
 		for(var k=mRAIT; k<mRAIT+7;k++){
